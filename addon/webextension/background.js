ec8b0f522d7bb705aea672152368a436d9f2bec7
Gregg Lind <gregg.lind@gmail.com>
2017-12-05 11:26:39 -0600

    WIP

--- addon/webextension/background.js
+++ addon/webextension/background.js
@@ -1,8 +1,7 @@
 "use strict";
 
 /** `background.js` example for embedded webExtensions.
-  * - As usual for webExtensions, controls BrowserAction (toolbar button)
-  *   look, feel, interactions.
+  * - As usual for webExtensions
   *
   * - Also handles 2-way communication with the HOST (Legacy Addon)
   *
@@ -11,7 +10,6 @@
   *   - Only the webExtension can initiate messages.  see `msgStudy('info')` below.
   */
 
-
 async function isEmbedded () {
   let ans;
   return msgHost("embedded?")
@@ -19,16 +17,11 @@ async function isEmbedded () {
     .catch( () => false )
 }
 
-/**  Re-usable code for talking to `studyUtils` using `browser.runtime.sendMessage`
+/**  Re-usable code for talking to the chrome privileged addon host` using `browser.runtime.sendMessage`
+  *
   *  - Host listens and responds at `bootstrap.js`:
   *
   *   `browser.runtime.onMessage.addListener(studyUtils.respondToWebExtensionMessage)`;
-  *
-  *  - `msg` calls the corresponding studyUtils API call.
-  *
-  *     - info: current studyUtils configuration, including 'variation'
-  *     - endStudy: for ending a study
-  *     - telemetry: send a 'shield-study-addon' packet
   */
 async function msgHost(msg) {
  try {
@@ -50,12 +43,6 @@ class AddHeaderForSpecialPage {
 
 }
 
-/** CONFIGURE and INSTRUMENT the BrowserAction button for a specific variation
- *
- *  1. Request 'info' from the hosting Legacy Extension.
- *  2. We only care about the `variation` key.
- *  3. initialize the feature, using our specific variation
- */
 async function runOnce() {
   const embedded = await isEmbedded();
   console.log("Embedded?", embedded);
