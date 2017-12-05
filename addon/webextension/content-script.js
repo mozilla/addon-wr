eec35625986db54b6d2f1e184eca7d437dd58004
Bianca Danforth <biancadanforth@gmail.com>
2017-12-05 12:00:00 -0800

    Add styles to each content page
    Still needs recursive text replace

--- addon/webextension/content-script.js
+++ addon/webextension/content-script.js
@@ -1,1 +1,7 @@
-console.log("test");
+// jquery calls this function when page content "onReady" event fires
+const $ = jQuery;
+
+$(function doOnReady() {
+  console.log("onReady fired");
+  $("div").replaceText("Dark", "light");
+});
