bbedf97b8d3992208c356b886c09c00bd5449b79
Gregg Lind <gregg.lind@gmail.com>
2017-12-06 15:33:46 -0600

    Fix #11:  Pref now works on and off.

--- addon/bootstrap.js
+++ addon/bootstrap.js
@@ -1,10 +1,14 @@
 "use strict";
 
-/* global Feature, Services */ // Cu.import
+/* global Services */ // Cu.import
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "(startup|shutdown|install|uninstall)", "argsIgnorePattern": "^_" }]*/
 
 const { utils: Cu } = Components;
 Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "LegacyExtensionsUtils",
+  "resource://gre/modules/LegacyExtensionsUtils.jsm");
+
 const prefs = Services.prefs;
 
 // our pref
@@ -14,14 +18,21 @@ if (!prefs.prefHasUserValue(PREFNAME)) {
 }
 
 async function startup(addonData, _reason) {
-  const { webExtension } = addonData;
-
   // set a watcher for that pref.
   // https://developer.mozilla.org/en-US/docs/Mozilla/Tech/XPCOM/Reference/Interface/nsIPrefBranch
   prefs.addObserver(PREFNAME, (_aSubject, _aTopic, _aData) => {
     const userOptedIn = prefs.getBoolPref(PREFNAME, false);
-    if (userOptedIn) webExtension.startup();
-    else webExtension.shutdown();
+
+    /* per https://dxr.mozilla.org/mozilla-central/source/toolkit/mozapps/extensions/internal/XPIProvider.jsm#4387-4400
+     * only 'startup' is exposed, not 'shutdown'
+     *
+     * use this method:
+     * - https://searchfox.org/mozilla-central/source/browser/extensions/screenshots/bootstrap.js#168
+     */
+    const realWebExtension = LegacyExtensionsUtils.getEmbeddedExtensionFor(addonData);
+
+    if (userOptedIn) realWebExtension.startup();
+    else realWebExtension.shutdown();
   });
 
   // IFF the pref is set, startup
